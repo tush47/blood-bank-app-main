5ee96e695b395340f785dee1e791c68b22c6994f
tush47 <tshulk2003@gmail.com>
2025-07-18 13:38:45 +0530

    cors all restrictions removed

--- server/server.js
+++ server/server.js
@@ -11,13 +11,14 @@ const app = express();
 
 // Apply middlewares
 app.use(express.json());
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL || 'https://blood-bank-app-main.vercel.app',
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: process.env.CLIENT_URL || 'https://blood-bank-app-main.vercel.app',
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
+app.use(cors());
 app.use(morgan("dev"));
 
 // Define routes
