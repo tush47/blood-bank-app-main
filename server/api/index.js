805616ce52eafcd5d1f0fba386d666b7b9c0b4d8
Tushar Sohal <110597823+tush47@users.noreply.github.com>
2025-07-29 15:24:08 +0530

    Update index.js options req added

--- server/api/index.js
+++ server/api/index.js
@@ -33,7 +33,7 @@ app.use(
         callback(new Error('Not allowed by CORS'));
       }
     },
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE","OPTIONS"],
     credentials: true,
   })
 );
